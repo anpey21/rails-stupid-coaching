214f6105edb4cc8e49d8320c677655aa5d8c10d2
Anna Peysikova <anpey21@gmail.com>
2023-02-20 11:58:20 +0800

    second

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+# What is a frozen string literal?
+# https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Style/FrozenStringLiteralComment
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
 end
